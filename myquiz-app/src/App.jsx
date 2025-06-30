aec35c5ce8c307a5e0e93bafd8c22d60e3cb775c
swamy-pega <swamy.p@hotmail.com>
2025-06-30 17:20:54 -0400

    test

--- myquiz-app/src/App.jsx
+++ myquiz-app/src/App.jsx
@@ -1,23 +1,13 @@
 
-//import ListGroup from "./component/ListGroup"
-//import MovieResults from "./component/MoviesSearch.jsx"
+
 import React from "react";
 import Home from "./component/Home.jsx"
-//import Services from "./component/Services.jsx"
-//import Contact from "./component/Contact.jsx"
+
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navbar from './component/Navbar.jsx'
-//import Products from "./Products.jsx"
-//import HeaderMsg from "./component/jsfunctions.js"
-//import Gallery from './Gallery.js';
 
-//import PropsTest from "./component/PropsTest.jsx";
-//import CalcCartTotal from "./component/CartTotal.tsx";
-//FAQList
-//import FAQList from "./component/QuestionsList.tsx";
 import Quiz from "./component/QuizSample2.tsx";
-//import SingleSelectButtons from "./component/multipleButtons.tsx";
-//import ExcelReader from "./component/UploadFile.jsx"
+
 function App() {
 
     /* <div><Quiz/></div> 
--- myquiz-app/src/component/Home.jsx
+++ myquiz-app/src/component/Home.jsx
@@ -6,7 +6,7 @@ const Home= ()=>
 
     const handleContinue=()=>
     {
-navigate('/Quiz');
+navigate('/quiz-app');
 
     }
 
