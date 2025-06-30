e2ce9c01026b865894da34cb483e7df73fff8e62
swamy-pega <swamy.p@hotmail.com>
2025-06-30 13:46:05 -0400

    url up

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:8000",  "http://localhost:8000",
-    "http://www.swamy-p.xyz","https://pythonproj-swamy-6ac538a29b8b.herokuapp.com"
+    "http://www.swamy-p.xyz", "https://www.swamy-p.xyz","https://pythonproj-swamy-6ac538a29b8b.herokuapp.com"
 
 ]
 app.add_middleware(
